7506b4ee9ea0339699736977bfed2ce2224712e3
christophermarek <christophermarek42@gmail.com>
2021-01-15 21:30:48 -0500

    added Map preset

--- terra/src/App.js
+++ terra/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import MapEditor from './components/Map/MapEditor';
 import Simulation from './components/Simulation/Simulation';
+import { Map1, Map1Ai } from './data/map/map1preset';
 
 import {
   BrowserRouter as Router,
@@ -12,49 +13,62 @@ import {
 
 
 function App() {
-  
+
   const [selectedNavItem, setSelectedNavItem] = useState('');
 
   useEffect(() => {
     const pathname = window.location.pathname
-    if(pathname.toString().toLowerCase() === '/editor'){
+    if (pathname.toString().toLowerCase() === '/editor') {
       setSelectedNavItem('editor');
     }
-    if(pathname.toString().toLowerCase() === '/simulation' || pathname.toString().toLowerCase() === '/'){
+    if (pathname.toString().toLowerCase() === '/simulation' || pathname.toString().toLowerCase() === '/') {
       setSelectedNavItem('simulation');
     }
+
+    //check if its the first time the user has been on
+    if(!localStorage.hasOwnProperty(`map1`)){
+      //get map data and brain from file
+      localStorage.setItem('map1', JSON.stringify(Map1));
+      localStorage.setItem('map1Ai', JSON.stringify(Map1Ai));
+      //set map and brain data to map1 from local storage
+    }
+
   });
+
   
 
-  function navButtonClicked(routeName){
+
+
+
+  function navButtonClicked(routeName) {
     setSelectedNavItem(routeName);
   }
 
   return (
     <div className="App">
       <Router>
-      <div>
-        <nav className="navBar">
-              <p className="navItem">Terra</p>
-              <Link className={"navItem navBtn" + (selectedNavItem === 'simulation' ? ' selectedButtonNoBorder' : ' ')} to="/Simulation" 
-                    onClick={() => navButtonClicked('simulation')}>Simulation</Link>
-              <Link className={"navItem navBtn" + (selectedNavItem === 'editor' ? ' selectedButtonNoBorder' : ' ')} to="/Editor"
-                    onClick={() => navButtonClicked('editor')}>Editor</Link>
-        </nav>
-        <Switch>
-          <Route path="/simulation">
-            <Simulation />
-          </Route>
-          <Route path="/editor">
-            <MapEditor />
-          </Route>
-          <Route path="/" >
-            <Simulation />
-          </Route>
+        <div>
+          <nav className="navBar">
+            <p className="navItem">Terra</p>
+            <Link className={"navItem navBtn" + (selectedNavItem === 'simulation' ? ' selectedButtonNoBorder' : ' ')} to="/Simulation"
+              onClick={() => navButtonClicked('simulation')}>Simulation</Link>
+            <Link className={"navItem navBtn" + (selectedNavItem === 'editor' ? ' selectedButtonNoBorder' : ' ')} to="/Editor"
+              onClick={() => navButtonClicked('editor')}>Editor</Link>
+          </nav>
+          <Switch>
+            <Route path="/simulation">
+              <Simulation />
+            </Route>
+            <Route path="/editor">
+              <MapEditor />
+            </Route>
+            <Route path="/" >
+              <Simulation />
+            </Route>
 
-        </Switch>
-      </div>
-    </Router>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
